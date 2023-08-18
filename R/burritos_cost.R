6205e4a1680b23d0ddbe071032b8b7cd49009b47
kristinaglass <kristinaglass48@gmail.com>
2023-08-18 14:29:08 -0700

    Initial commit

---
+++ R/burritos_cost.R
@@ -0,0 +1,13 @@
+#' Calculate Total Burrito Cost
+#'
+#' @param price Price per burrito in dollars
+#' @param count Price per burrito in dollars
+#'
+#' @return
+#' @export
+#'
+#' @examples
+burritos_cost <- function(price, count) {
+  price * count
+}
+
---
+++ R/icecream_cost.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param price cost of ice cream
+#' @param count number of ice cream
+#'
+#' @return
+#' @export
+#'
+#' @examples
+icecream_cost <- function(price, count) {
+  price * count
+}
